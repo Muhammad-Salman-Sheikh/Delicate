60a1a7d4639eee8d9b1e7ab3e1207409f524ca9f
Muhammad-Salman-Sheikh <37584841+Muhammad-Salman-Sheikh@users.noreply.github.com>
2018-04-27 16:32:16 +0300

    Update parser.js

--- parser.js
+++ parser.js
@@ -1,3 +1,19 @@
 /**
-	* 
+	* The parser constructor
+	* @param {object} functions
+	* @param {tokens} tokens
+	
 **/
+const Parser = function(functions , tokens) {
+	this.functions = functions;
+	this.tokens = tokens;
+	this.parse = () => this.tokens.length == 0 ?
+			(() => { // If no operator is present
+				type : 'noop'
+			})() : 
+		this.tokens[0] == '->' ?
+			(() => {
+				
+			})() : 1; //WIP
+	
+};
